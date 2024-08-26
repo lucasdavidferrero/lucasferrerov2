f7a73a0cb2e86dc7b84f4a3f981c10d43383ac6a
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-08-26 18:57:04 -0300

    Agrego como me gusta a mi el accent. Tomar colores recomendados y estandarizar..

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,11 @@ export default {
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
+        display: ['Orbitron', 'sans-serif']
+      },
+      colors: {
+        'burnt-orange': '#ff8c42',
+        'gold': '#ffd700',
       }
     },
   },
